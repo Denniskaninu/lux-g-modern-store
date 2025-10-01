b1b81a95e7890f1f65ef9b6424dd1de8b56a10f3
Dennis Kaninu <denniskaninu254@gmail.com>
2025-10-01 18:31:37 +0000

    stop that, lets solve this error issue of content-script.js:22 Document

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,9 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
-// Your web app's Firebase configuration
-// Use NEXT_PUBLIC_ prefixed variables to ensure they are available on both client and server.
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-};
+const firebaseConfig = require('../../firebase.config.js');
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
